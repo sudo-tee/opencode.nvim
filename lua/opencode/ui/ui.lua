b41a4df3d71536e8e05da49d9569cc647d877e4f
Cameron Ring <cameron@cs.stanford.edu>
2025-10-12 15:10:04 -0700

    chore(ui): find better way to call render markdown

--- lua/opencode/ui/ui.lua
+++ lua/opencode/ui/ui.lua
@@ -17,6 +17,8 @@ function M.scroll_to_bottom()
     end)
   end)
 
+  -- TODO: shouldn't have hardcoded calls to render_markdown,
+  -- should support user callbacks
   vim.defer_fn(function()
     renderer.render_markdown()
   end, 200)
@@ -198,15 +200,15 @@ end
 --   renderer.render_incremental(state.windows, message)
 -- end
 
-function M.render_lines(lines)
-  M.clear_output()
-  renderer.write_output(state.windows, lines)
-  renderer.render_markdown()
-end
+-- function M.render_lines(lines)
+--   M.clear_output()
+--   renderer.write_output(state.windows, lines)
+--   renderer.render_markdown()
+-- end
 
-function M.stop_render_output()
-  renderer.stop()
-end
+-- function M.stop_render_output()
+--   renderer.stop()
+-- end
 
 function M.select_session(sessions, cb)
   local session_picker = require('opencode.ui.session_picker')

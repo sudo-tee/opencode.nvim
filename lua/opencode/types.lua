50f922dc6861f1f0f2af5771932fa22630fbf053
Francis Belanger <francis.belanger@gmail.com>
2025-10-16 15:27:29 -0400

    fix: issues with backmerge

--- lua/opencode/types.lua
+++ lua/opencode/types.lua
@@ -53,7 +53,8 @@
 ---@field share? SessionShareInfo
 
 ---@class OpencodeKeymapEntry
----@field [1] string # Function name
+---@field [1] string # Function nam
+
 ---@field mode? string|string[] # Mode(s) for the keymap
 ---@field desc? string # Keymap description
 
@@ -66,28 +67,6 @@
 ---@field accept_all string
 ---@field deny string
 
-<<<<<<< HEAD
-=======
----@class OpencodeKeymapWindow
----@field submit string
----@field submit_insert string
----@field close string
----@field stop string
----@field next_message string
----@field prev_message string
----@field mention_file string # mention files with a file picker
----@field mention string # mention subagents or files with a completion popup
----@field slash_commands string
----@field context_items string # select context items with a completion popup
----@field toggle_pane string
----@field prev_prompt_history string
----@field next_prompt_history string
----@field focus_input string
----@field debug_message string
----@field debug_output string
----@field switch_mode string
----@field select_child_session string
->>>>>>> 31b9b62 (wip: context bar)
 ---@class OpencodeKeymap
 ---@field editor OpencodeKeymapEditor
 ---@field input_window OpencodeKeymapInputWindow
@@ -140,13 +119,8 @@
 ---@class OpencodeContextConfig
 ---@field enabled boolean
 ---@field cursor_data { enabled: boolean }
-<<<<<<< HEAD
----@field diagnostics { info: boolean, warning: boolean, error: boolean }
----@field current_file { enabled: boolean, show_full_path: boolean }
-=======
 ---@field diagnostics { enabled:boolean, info: boolean, warning: boolean, error: boolean }
 ---@field current_file { enabled: boolean }
->>>>>>> 31b9b62 (wip: context bar)
 ---@field selection { enabled: boolean }
 ---@field subagents { enabled: boolean }
 

9692c0a7d57b6ab9b36df22c8d6a96ccfbf0e877
Cameron Ring <cameron@cs.stanford.edu>
2025-10-23 07:40:38 -0700

    fix(init): don't need vim.schedule around setup
    Not wrapping means vim cmds will be defined by the end of the setup call
    which is more predictable (and also makes lazy loading much easier)

--- lua/opencode/init.lua
+++ lua/opencode/init.lua
@@ -1,20 +1,18 @@
 local M = {}
 
 function M.setup(opts)
-  vim.schedule(function()
-    -- Have to setup config first, especially before state as
-    -- it initializes at least one value (current_mode) from config.
-    -- If state is require'd first then it will not get what may
-    -- be set by the user
-    local config = require('opencode.config')
-    config.setup(opts)
+  -- Have to setup config first, especially before state as
+  -- it initializes at least one value (current_mode) from config.
+  -- If state is require'd first then it will not get what may
+  -- be set by the user
+  local config = require('opencode.config')
+  config.setup(opts)
 
-    require('opencode.core').setup()
-    require('opencode.api').setup()
-    require('opencode.keymap').setup(config.keymap)
-    require('opencode.ui.completion').setup()
-    require('opencode.event_manager').setup()
-  end)
+  require('opencode.core').setup()
+  require('opencode.api').setup()
+  require('opencode.keymap').setup(config.keymap)
+  require('opencode.ui.completion').setup()
+  require('opencode.event_manager').setup()
 end
 
 return M

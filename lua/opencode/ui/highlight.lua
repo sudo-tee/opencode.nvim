626098e43072c9f14fab1e088df41da2ca8aa43c
Francis Belanger <francis.belanger@gmail.com>
2025-12-22 09:23:07 -0500

    feat(reasoning): show reasoning output + toggle
    This PR add reasoning output to the panel.
    
    It cols be toggled with `/reasoning` or `/thinking`, `Opencode
    toggle_reasoning_output ` or `<leader>otr`
    
    This should resolve #138

--- lua/opencode/ui/highlight.lua
+++ lua/opencode/ui/highlight.lua
@@ -34,6 +34,7 @@ function M.setup()
     vim.api.nvim_set_hl(0, 'OpencodeContextSwitchOn', { link = '@label', default = true })
     vim.api.nvim_set_hl(0, 'OpencodePickerTime', { link = 'Comment', default = true })
     vim.api.nvim_set_hl(0, 'OpencodeDebugText', { link = 'Comment', default = true })
+    vim.api.nvim_set_hl(0, 'OpencodeReasoningText', { link = 'Comment', default = true })
   else
     vim.api.nvim_set_hl(0, 'OpencodeBorder', { fg = '#616161', default = true })
     vim.api.nvim_set_hl(0, 'OpencodeBackground', { link = 'Normal', default = true })
@@ -64,6 +65,7 @@ function M.setup()
     vim.api.nvim_set_hl(0, 'OpencodeContextSwitchOn', { link = '@label', default = true })
     vim.api.nvim_set_hl(0, 'OpencodePickerTime', { link = 'Comment', default = true })
     vim.api.nvim_set_hl(0, 'OpencodeDebugText', { link = 'Comment', default = true })
+    vim.api.nvim_set_hl(0, 'OpencodeReasoningText', { link = 'Comment', default = true })
   end
 end
 
